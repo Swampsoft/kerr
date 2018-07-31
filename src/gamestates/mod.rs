430e053925b8a10c83e1b8c3a95ccd71aa61164e
Martin Billinger <flkazemakase@gmail.com>
2018-07-31 23:55:39 +0200

    Basic state framework

---
+++ src/gamestates/mod.rs
@@ -0,0 +1,55 @@
+
+pub mod hello;
+
+use ggez::{Context, GameResult, event::EventHandler};
+
+pub trait GameState {
+    /// update. return `Ok(true)` if the state below should be updated too.
+    fn update(&mut self, ctx: &mut Context) -> GameResult<bool>;
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()>;
+
+    /// override if this state is not completete opaque and the previous state should be drown too
+    fn draw_previous(&self) -> bool { false }
+}
+
+pub struct StateManager {
+    states: Vec<Box<GameState>>,
+}
+
+impl StateManager {
+    pub fn new<T: GameState + 'static>(initial_state: T) -> Self {
+        StateManager {
+            states: vec![Box::new(initial_state)]
+        }
+    }
+
+    fn draw_recursive(states: &mut [Box<GameState>], ctx: &mut Context) -> GameResult<()> {
+        if let Some((last, prev_states)) = states.split_last_mut() {
+            if last.draw_previous() {
+                StateManager::draw_recursive(prev_states, ctx)?;
+            }
+            last.draw(ctx)?;
+        }
+        Ok(())
+    }
+}
+
+impl EventHandler for StateManager {
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        if self.states.is_empty() {
+            return ctx.quit()
+        }
+
+        for state in self.states.iter_mut().rev() {
+            if !state.update(ctx)? {
+                break
+            }
+        }
+
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        StateManager::draw_recursive(&mut self.states, ctx)
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,21 @@
+extern crate ggez;
+
+mod gamestates;
+
+use ggez::{conf, GameResult};
+
+use gamestates::{hello::HelloState, StateManager};
+
+fn main() -> GameResult<()> {
+    let c = conf::Conf {
+        window_mode: conf::WindowMode::default().dimensions(1024, 768),
+        window_setup: conf::WindowSetup::default().title("Rock Project"),
+        backend: conf::Backend::OpenGL { major: 3, minor: 2 },
+    };
+
+    let ctx = &mut ggez::Context::load_from_conf("Kerr", "Swampsoft Games", c).unwrap();
+
+    let main_state = &mut StateManager::new(HelloState::new(ctx)?);
+
+    ggez::event::run(ctx, main_state)
+}
